2e4fca0ced2a15b08114c393696bf7e4c77219df
fiorenzo <fiorenzino@gmail.com>
2014-02-10 00:20:14 +0100

    java cdi @inject

---
+++ src/main/java/org/plivo/ee/inject/notification/Copy_8_of_DialALegUUID.java
@@ -0,0 +1,18 @@
+package org.plivo.ee.inject.notification;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Retention(RUNTIME)
+@Target({ METHOD, FIELD, PARAMETER })
+public @interface Copy_8_of_DialALegUUID {
+
+}
